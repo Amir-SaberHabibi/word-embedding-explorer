e80ffd6f30a62e19d2de017ccb0714590f92e3e3
Amir-SaberHabibi <amirsaberhabibi80@gmail.com>
2024-09-03 17:10:42 +0330

    changes

--- src/homepage.py
+++ src/homepage.py
@@ -1,7 +1,8 @@
-# tab1_home.py
-
 import streamlit as st
 from components import type_text
+
+with st.sidebar:
+    st.write('A sample text')
 
 def show_home():
     st.markdown("<h3>Welcome to the advanced word embeddings vectorization tool!</h3>", unsafe_allow_html=True)
